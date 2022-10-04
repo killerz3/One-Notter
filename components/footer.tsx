39a986d18f9a1cb91a790e36662b2059b8461cf6
Shubhveer Singh Chaudhary <shubhchaudhary1203@gmail.com>
2022-10-05 00:09:30 +0530

    db and theme setup , basic auth0 config

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+import {RiCopyrightLine} from 'react-icons/ri'
+
+
+export default function Footer() {
+    
+
+    return (
+        <div className='flex items-baseline bg-neutral-100 dark:bg-stone-900
+         text-black dark:text-neutral-100 justify-center'>
+            <RiCopyrightLine className='w-5'/>  {new Date().getFullYear().toString()} <span> &nbsp; Created by Shubhveer Singh Chaudhary </span>
+        </div>
+        )
+
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
+import Layout from '../components/layout'
+import { UserProvider } from '@auth0/nextjs-auth0'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute='class' >
+      <UserProvider>
+
+      <Layout>
+
+      <Component {...pageProps} />
+      </Layout>
+      </UserProvider>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
